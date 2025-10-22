6632e51285fe0ed39b3d87cc8cd2ed75810f29f0
akdeb <deb2@illinois.edu>
2025-10-23 00:57:10 +0100

    set volume to 20

--- firmware-arduino/src/Config.cpp
+++ firmware-arduino/src/Config.cpp
@@ -33,11 +33,11 @@ volatile bool sleepRequested = false;
  */
 
 #ifdef DEV_MODE
-const char *ws_server = "10.2.1.195";
+const char *ws_server = "192.168.68.119";
 const uint16_t ws_port = 8000;
 const char *ws_path = "/";
 // Backend server details 
-const char *backend_server = "10.2.1.195";
+const char *backend_server = "192.168.68.119";
 const uint16_t backend_port = 3000;
 
 #elif defined(PROD_MODE)
